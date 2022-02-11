fe8592f07841f9c3257b7f011349380c254d126c
fenix144 <yan_1731@hotmail.com>
2022-02-11 12:00:54 -0500

    Added and changed prototypes

--- push_swap.h
+++ push_swap.h
@@ -2,15 +2,15 @@
 
 int		intcheck(char **str);
 int		*buildstack(char *argv[], int *stack, int argc);
-int		push_swap(int *stack_a, int *stack_b, int argc);
+void	push_swap(int *stack_a, int *stack_b, int argc);
 int		checkdupe(int *stack, int argc);
 void	errorhandlingint(char *argv[]);
 void	errorhandlingstack(int *stack_a, int *stack_b);
 void	shiftstackup(int *stack, int *stacksize);
 void	shiftstackdown(int *stack, int *stacksize);
-void	sa(int *stack_a);
-void	sb(int *stack_b);
-void	ss(int *stack_a, int *stack_b);
+void	sa(int *stack_a, int stack_a_size);
+void	sb(int *stack_b, int stack_b_size);
+void	ss(int *stack_a, int *stack_b, int stack_a_size, int stack_b_size);
 void	ra(int *stack_a, int *stack_a_size);
 void	rb(int *stack_b, int *stack_b_size);
 void	rr(int *stack_a, int *stack_b, int *stack_a_size, int *stack_b_size);
