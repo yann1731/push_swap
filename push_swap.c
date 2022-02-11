9d1f2e084dda076e45ce8606a3e616eb4bcdb6ac
fenix144 <yan_1731@hotmail.com>
2022-02-11 12:00:31 -0500

    Mostly done

--- push_swap.c
+++ push_swap.c
@@ -1,11 +1,12 @@
 #include "libft.h"
 #include "push_swap.h"
 
-int	push_swap(int *stack_a, int *stack_b, int argc)
+void	push_swap(int *stack_a, int *stack_b, int argc)
 {
 	int stack_a_size;
 	int stack_b_size;
 
 	stack_a_size = argc - 1;
 	stack_b_size = 0;
+	sorting(stack_a, stack_b, stack_a_size, stack_b_size);
 }
