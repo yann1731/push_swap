bd815ab3189b73c28080117f2a32d3f7e9236ba6
fenix144 <yan_1731@hotmail.com>
2022-02-19 17:18:28 -0500

    Might need that for proper sorting

--- bubblesort.c
+++ bubblesort.c
@@ -1,3 +1,21 @@
 #include "libft.h"
 #include "push_swap.h"
 
+void bubble_sort(int *sorted_stack, int stack_size)
+{
+	int	i;
+	int j;
+
+	i = 0;
+	j = 0;
+	while (is_sorted(sorted_stack, stack_size) == 0)
+	{
+		while (j < (stack_size - 1))
+		{
+			if (sorted_stack[j] > sorted_stack[j + 1])
+				swap(&sorted_stack[j], &sorted_stack[j + 1]);
+		j++;
+		}
+	j = 0;
+	}
+}
