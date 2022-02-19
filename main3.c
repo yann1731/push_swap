6c0c4f349444d048aa5ad127078d5b508fb5816e
fenix144 <yan_1731@hotmail.com>
2022-02-19 17:18:53 -0500

    Still testing

--- main3.c
+++ main3.c
@@ -1,30 +1,28 @@
 #include "libft.h"
-#include "push_swap.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include "libft.h"
 
-void	shiftstackdown(int *stack, int *stacksize)
+void	shiftstackdown(int *stack, int stacksize)
 {
 	int index;
 
-	index = *stacksize;
+	index = stacksize;
 
 	while (index > 0)
 	{
 		*(stack + index) = *(stack + (index - 1));
 		index--;
 	}
-	*(stack + index) = 0;
 }
 
-void	shiftstackup(int *stack, int *stacksize)
+void	shiftstackup(int *stack, int stacksize)
 {
 	int stop;
 	int index;
 
-	stop = *stacksize;
+	stop = stacksize;
 	index = 0;
 
 	while (index < stop)
@@ -38,9 +36,9 @@ void	pa(int *stack_a, int *stack_b, int *stack_a_size, int *stack_b_size)
 {
 	if (*stack_b_size != 0)
 	{
-		shiftstackdown(stack_a, stack_a_size);
+		shiftstackdown(stack_a, *stack_a_size);
 		*stack_a = *stack_b;
-		shiftstackup(stack_b, stack_b_size);
+		shiftstackup(stack_b, *stack_b_size);
 		*stack_a_size += 1;
 		*stack_b_size -= 1;
 		write(1, "pa\n", 3);
@@ -51,63 +49,63 @@ void	pb(int *stack_a, int *stack_b, int *stack_a_size, int *stack_b_size)
 {
 	if (*stack_a_size != 0)
 	{
-		shiftstackdown(stack_b, stack_b_size);
+		shiftstackdown(stack_b, *stack_b_size);
 		*stack_b = *stack_a;
-		shiftstackup(stack_a, stack_a_size);
+		shiftstackup(stack_a, *stack_a_size);
 		*stack_b_size += 1;
 		*stack_a_size -= 1;
 		write(1, "pb\n", 3);
 	}
 }
 
-void	ra(int *stack_a, int *stack_a_size)
+void	ra(int *stack_a, int stack_a_size)
 {
 	int temp;
 
 	temp = *stack_a;
 	shiftstackup(stack_a, stack_a_size);
-	*(stack_a + *stack_a_size) = temp;
+	*(stack_a + (stack_a_size - 1)) = temp;
 	write(1, "ra\n", 3);
 }
 
-void	rb(int *stack_b, int *stack_b_size)
+void	rb(int *stack_b, int stack_b_size)
 {
 	int temp;
 
 	temp = *stack_b;
 	shiftstackup(stack_b, stack_b_size);
-	*(stack_b + *stack_b_size) = temp;
+	*(stack_b + (stack_b_size - 1)) = temp;
 	write(1, "rb\n", 3);
 }
 
-void	rr(int *stack_a, int *stack_b, int *stack_a_size, int *stack_b_size)
+void	rr(int *stack_a, int *stack_b, int stack_a_size, int stack_b_size)
 {
 	ra(stack_a, stack_a_size);
 	rb(stack_b, stack_b_size);
 	write(1, "rr\n", 3);
 }
 
-void	rra(int *stack_a, int *stack_a_size)
+void	rra(int *stack_a, int stack_a_size)
 {
 	int temp;
 
-	temp = *(stack_a + *stack_a_size);
+	temp = *(stack_a + (stack_a_size - 1));
 	shiftstackdown(stack_a, stack_a_size);
 	*(stack_a) = temp;
 	write(1, "rra\n", 4);
 }
 
-void	rrb(int *stack_b, int *stack_b_size)
+void	rrb(int *stack_b, int stack_b_size)
 {
 	int temp;
 
-	temp = *(stack_b + *stack_b_size);
+	temp = *(stack_b + stack_b_size);
 	shiftstackdown(stack_b, stack_b_size);
 	*(stack_b) = temp;
 	write(1, "rrb\n", 4);
 }
 
-void	rrr(int *stack_a, int *stack_b, int *stack_a_size, int *stack_b_size)
+void	rrr(int *stack_a, int *stack_b, int stack_a_size, int stack_b_size)
 {
 	rra(stack_a, stack_a_size);
 	rrb(stack_b, stack_b_size);
@@ -162,6 +160,25 @@ int	is_sorted(int *stack_a, int stack_a_size)
 	return (1);
 }
 
+int	is_rsorted(int *stack_b, int stack_b_size)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = i + 1;
+	if (stack_b_size < 2)
+		return (0);
+	while (j < stack_b_size)
+	{
+		if (stack_b[i] < stack_b[j])
+			return (0);
+		i++;
+		j++;
+	}
+	return (1);
+}
+
 int	checkdupe(int *stack, int argc)
 {
 	int i;
@@ -194,6 +211,25 @@ int *buildstack(char *argv[], int *stack, int argc)
 		stack[j++] = ft_atoi(argv[i]);
 	if (!checkdupe(stack, argc))
 	{
+		free(stack);
+		write(2, "Error\n", 6);
+		exit(0);
+	}
+	return (stack);
+}
+
+int *buildstack_single(char *argv[], int *stack, int argc)
+{
+	int i;
+	int j;
+
+	i = -1;
+	j = 0;
+	while (argv[++i])
+		stack[j++] = ft_atoi(argv[i]);
+	if (!checkdupe(stack, argc))
+	{
+		free(stack);
 		write(2, "Error\n", 6);
 		exit(0);
 	}
@@ -230,25 +266,153 @@ void	errorhandlingstack(int *stack_a, int *stack_b)
 {
 	if (stack_a == NULL || stack_b == NULL)
 	{
-		if (stack_a == NULL)
-		{
-			write(2, "Error\n", 6);
-			exit(0);
-		}
-		if (stack_b == NULL)
-		{
-			write(2, "Error\n", 6);
-			exit(0);
-		}
+		free(stack_a);
+		free(stack_b);
+		write(2, "Error\n", 6);
+		exit(0);
 	}
+}
+
+int	argcount(char **str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i + 1);
+}
+
+char	**handle_single_string(char *argv[], int *argc)
+{
+	char **str;
+	int	i;
+
+	i = -1;
+	str = ft_split(argv[1], ' ');
+	errorhandlingint(str);
+	*argc = argcount(str);
+	return (str);
+}
+
+void swap(int *a, int *b)
+{
+	int	tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+void bubble_sort(int *sorted_stack, int stack_size)
+{
+	int	i;
+	int j;
+
+	i = 0;
+	j = 0;
+	while (is_sorted(sorted_stack, stack_size) == 0)
+	{
+		while (j < (stack_size - 1))
+		{
+			if (sorted_stack[j] > sorted_stack[j + 1])
+				swap(&sorted_stack[j], &sorted_stack[j + 1]);
+		j++;
+		}
+	j = 0;
+	}
+}
+
+// Create a temporary stack say tmpStack.
+// While input stack is NOT empty do this:
+// Pop an element from input stack call it temp
+// while temporary stack is NOT empty and top of temporary stack is greater than temp,
+// pop from temporary stack and push it to the input stack
+// push temp in temporary stack
+// The sorted numbers are in tmpStack
+
+// First loop attempt: (is_sorted(stack_a, stack_a_size) == 0 || stack_b_size != 0)
+// Sequence used: 4 6 1 3 7 5 15 11 13
+// while (is_sorted(stack_a, stack_a_size) == 0)
+// 	{
+// 		while (stack_a_size > 0)
+// 		{
+// 			if (stack_a[0] > stack_a[1] && stack_a_size >= 2)
+// 				sa(stack_a, stack_a_size);
+// 			pb(stack_a, stack_b, &stack_a_size, &stack_b_size);
+// 			if (stack_b[0] < stack_b[1] && stack_b_size >= 2)
+// 				sb(stack_b, stack_b_size);
+// 		}
+// 		while (stack_b_size > 0)
+// 		{
+// 			if (stack_b[0] < stack_b[1] && stack_b_size >= 2)
+// 				sb(stack_b, stack_b_size);
+// 			pa(stack_a, stack_b, &stack_a_size, &stack_b_size);
+// 			if (stack_a[0] > stack_a[1] && stack_b_size >= 2)
+// 				sa(stack_a, stack_a_size);
+// 		}
+// 	}
+
+// int	half;
+// 	int i;
+
+// 	bubble_sort(sorted_stack, stack_a_size);
+// 	i = 0;
+// 	while (stack_a_size > 0)
+// 	{
+// 		half = stack_a_size / 2;
+// 		if (findindex(stack_a, sorted_stack[i], stack_a_size) <= half)
+// 		{
+// 			while (stack_a[0] != sorted_stack[i])
+// 				ra(stack_a, stack_a_size);
+// 			pb(stack_a, stack_b, &stack_a_size, &stack_b_size);
+// 			i++;
+// 		}
+// 		if (findindex(stack_a, sorted_stack[i], stack_a_size) > half)
+// 		{
+// 			while (stack_a[0] != sorted_stack[i])
+// 				rra(stack_a, stack_a_size);
+// 			pb(stack_a, stack_b, &stack_a_size, &stack_b_size);
+// 			i++;
+// 		}
+// 	}
+// 	while (stack_b_size > 0)
+// 		pa(stack_a, stack_b, &stack_a_size, &stack_b_size);
+
+// while (i < max)
+// 	{
+// 		if (stack_a[0] == sorted_stack[i])
+// 		{
+// 			pb(stack_a, stack_b, &stack_a_size, &stack_b_size);
+// 			i++;
+// 		}
+// 		ra(stack_a, stack_a_size);
+// 	}
+// 	while (stack_b_size > 0)
+// 		pa(stack_a, stack_b, &stack_a_size, &stack_b_size);
+
+void	small_sort(int *stack_a, int *stack_b, int stack_a_size, int stack_b_size)
+{
+	if (stack_a[0] > stack_a[1] && stack_a[1] )
+	
+}
+
+int	findindex(int *stack_a, int tofind, int stack_a_size)
+{
+	int	i;
+
+	i = -1;
+	while (++i < stack_a_size)
+		if (stack_a[i] == tofind)
+			return (i);
+	return (0);
 }
 
 void	sorting(int *stack_a, int *stack_b, int stack_a_size, int stack_b_size)
 {
-	while (is_sorted(stack_a, stack_a_size) == 0 && stack_b_size != 0)
-	{
-		
-	}
+	if (stack_a_size <= 3)
+		small_sort(stack_a, stack_b, stack_a_size, stack_b_size);
+	
 }
 
 void	push_swap(int *stack_a, int *stack_b, int argc)
@@ -263,15 +427,26 @@ void	push_swap(int *stack_a, int *stack_b, int argc)
 
 int main(int argc, char *argv[])
 {
-	int *stack_a;
-	int	*stack_b;
-	
+	int 	*stack_a;
+	int		*stack_b;
+	char	**str;
+
+	if (argc == 1)
+		exit(0);
+	if (argc > 2)
+	{
+		stack_a = malloc((argc - 1) * sizeof(int));
+		stack_a = buildstack(argv, stack_a, argc);
+	}
+	if (argc == 2)
+	{
+		argv = handle_single_string(argv, &argc);
+		stack_a = malloc((argc - 1) * sizeof(int));
+		stack_a = buildstack_single(argv, stack_a, argc);
+	}
 	errorhandlingint(argv);
-	stack_a = malloc((argc - 1) * sizeof(int));
 	stack_b = malloc((argc - 1) * sizeof(int));
 	errorhandlingstack(stack_a, stack_b);
-	stack_a = buildstack(argv, stack_a, argc);
 	push_swap(stack_a, stack_b, argc);
-
 	return (0);
 }
