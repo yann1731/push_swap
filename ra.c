e046593b8b789ac771a56a42851933cc9286fc61
fenix144 <yan_1731@hotmail.com>
2022-02-19 17:20:16 -0500

    Fixed

--- ra.c
+++ ra.c
@@ -1,12 +1,12 @@
 #include "libft.h"
 #include "push_swap.h"
 
-void	ra(int *stack_a, int *stack_a_size)
+void	ra(int *stack_a, int stack_a_size)
 {
 	int temp;
 
 	temp = *stack_a;
 	shiftstackup(stack_a, stack_a_size);
-	*(stack_a + *stack_a_size) = temp;
+	*(stack_a + stack_a_size) = temp;
 	write(1, "ra\n", 3);
 }
--- rrb.c
+++ rrb.c
@@ -1,11 +1,11 @@
 #include "libft.h"
 #include "push_swap.h"
 
-void	rrb(int *stack_b, int *stack_b_size)
+void	rrb(int *stack_b, int stack_b_size)
 {
 	int temp;
 
-	temp = *(stack_b + *stack_b_size);
+	temp = *(stack_b + stack_b_size);
 	shiftstackdown(stack_b, stack_b_size);
 	*(stack_b) = temp;
 	write(1, "rrb\n", 4);
