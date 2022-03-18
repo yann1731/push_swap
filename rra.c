95daac90dd372d40c329d395d8fb51cd5fc78cd1
fenix144 <yan_1731@hotmail.com>
2022-03-18 11:41:14 -0400

    Fixed indexing issue

--- rra.c
+++ rra.c
@@ -16,7 +16,7 @@ void	rra(int *stack_a, int stack_a_size)
 {
 	int	temp;
 
-	temp = *(stack_a + stack_a_size);
+	temp = *(stack_a + (stack_a_size - 1));
 	shiftstackdown(stack_a, stack_a_size);
 	*(stack_a) = temp;
 	write(1, "rra\n", 4);
